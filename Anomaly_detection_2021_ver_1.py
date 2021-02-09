2072b6e088ca11912ac3a593e30df5acfd564203
Mindaugas <mindaugas.bereisa@gmail.com>
2021-02-09 17:45:05 +0200

    2022 01 09 version

--- Anomaly_detection_2021_ver_1.py
+++ Anomaly_detection_2021_ver_1.py
@@ -17,7 +17,7 @@ import shutil
 warnings.filterwarnings("ignore")
 import time
 import plotly.express as px
-import io
+import os
 import matplotlib as mpl
 from matplotlib.pylab import rcParams
 
@@ -296,12 +296,12 @@ min = 5
 
 # Print charts all in one an 3d? ('y'/'n')
 print_all = 'y'
-show_chart = 'n'
+show_chart = 'y'
 save_chart = 'y'
 close_browser = 'n'
 # time and freqency parameters
 times_per_sek= 100
-close_seconds = 30
+close_seconds = 15
 
 print(
     '------------------------------------------------------------------------------------------------------------------------------------------')
